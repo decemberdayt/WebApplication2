3834683efb4665cd74282c828681c6e66db13610
decemberdayt <decemberdayt@users.noreply.github.com>
2021-01-07 21:22:14 +0100

    Dodaj pliki projektów.

---
+++ WebApplication2/Controllers/RoutesController.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class RoutesController : Controller
+    {
+        private TrackerEntities db = new TrackerEntities();
+
+        // GET: Routes
+        public ActionResult Index()
+        {
+            var route = db.Route.Include(r => r.AspNetUsers);
+            return View(route.ToList());
+        }
+
+        // GET: Routes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Route route = db.Route.Find(id);
+            if (route == null)
+            {
+                return HttpNotFound();
+            }
+            return View(route);
+        }
+
+        public Route FindMaxRouteId()
+        {
+            Route route = db.Route.OrderByDescending(u => u.RouteId).FirstOrDefault();
+            if(route==null)
+            {
+                route = new Route();
+                route.RouteId = 0;
+            }
+            return route;
+        }
+
+        // GET: Routes/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: Routes/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserId,Origin,OriginCoordinates,Destination,DestinationCoordinates")] Route route)
+        {
+            if (ModelState.IsValid)
+            {
+                route.RouteId = FindMaxRouteId().RouteId + 1;
+                db.Route.Add(route);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", route.UserId);
+            return View(route);
+        }
+
+        // główna funckja wywolywana z poziomu JS
+        [HttpPost]
+        public ActionResult CreateJS(String UserId, String Origin, String OriginCoordinates, String Destination, String DestinationCoordinates, String RouteLength)
+        {
+            Route route = new Route();
+            if (UserId != null)
+            {
+                route.RouteId = FindMaxRouteId().RouteId + 1;
+                route.OriginCoordinates = OriginCoordinates;
+                route.Origin = Origin;
+                route.RouteLength = RouteLength;
+                route.Destination = Destination;
+                route.DestinationCoordinates = DestinationCoordinates;
+                route.UserId = UserId;
+                db.Route.Add(route);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+        // GET: Routes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Route route = db.Route.Find(id);
+            if (route == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", route.UserId);
+            return View(route);
+        }
+
+        // POST: Routes/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "RouteId,UserId,Origin,OriginCoordinates,Destination,DestinationCoordinates")] Route route)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(route).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.AspNetUsers, "Id", "Email", route.UserId);
+            return View(route);
+        }
+
+        // GET: Routes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Route route = db.Route.Find(id);
+            if (route == null)
+            {
+                return HttpNotFound();
+            }
+            return View(route);
+        }
+
+        // POST: Routes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Route route = db.Route.Find(id);
+            db.Route.Remove(route);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebApplication2/Models/RouteViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class RouteViewModel
+    {
+        public int RouteId { get; set; }
+        public string UserId { get; set; }
+        public string Origin { get; set; }
+        public string OriginCoordinates { get; set; }
+        public string Destination { get; set; }
+        public string DestinationCoordinates { get; set; }
+        public string RouteName { get; set; }
+        public string RouteLength { get; set; }
+
+    }
+}
